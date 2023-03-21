b0f403f50fc6ec83b2c88f06b46ac735b3c7a1bc
Jesse_HP1 <agarjesse@gmail.com>
2023-03-21 11:08:34 -0400

    finish E1-04, add E1-04-B

--- 1-Easy-1-2-3/E1-04-RoomArea.js
+++ 1-Easy-1-2-3/E1-04-RoomArea.js
@@ -1,3 +1,12 @@
+let readlineSync = require('readline-sync');
+function roomSize() {
+  console.log('Room Width:');
+  let roomWidth = parseInt(readlineSync.prompt(""));
+  console.log('Room Height:');
+  let roomHeight = parseInt(readlineSync.prompt(""));
+  console.log(`The area of the room is ${(roomHeight * roomWidth).toFixed(2)} square meters (${((roomHeight * roomWidth) * 10.7639).toFixed(2)} square feet)`);
+}
+
 /* UNDERSTAND THE PROBLEM
   - read the problem description 
   - identify inputs/outputs
@@ -7,19 +16,19 @@
   - ask clarifying questions
   - examine all examples
 * INPUTS
-? 
+? 2 numbers, length and width of square in square metesr
 * OUTPUTS
-? 
-* RULES
-? 
-* REQUIREMENTS
-? 
+? 1 number, area in square meeters and square feet
+* RULES AND REQUIREMENTS
+? Don't have to validate input
+? 2 decimal places
 */
 /* EXAMPLES/TEST CASES
   - understand how the input translates to output
   - identify edge cases
   - create the test cases and confirm outputs
 * VALID CASES  */
+roomSize();
 
 // * EDGE CASES
 
@@ -27,7 +36,7 @@
   - what sort of actions do you have to do (sort, collect, filter, etc.)
   - what kind of data are you primarily dealing with? (strings, arrays, numbers, objects, etc.)
   - this helps you to focus on methods for these types
-? 
+? numbers
 
 */
 /* ALGORITHM
@@ -36,7 +45,7 @@
   - your code will depend on your algorithm
 ? 
 ?
-?
+?  
 */
 
 /* CODE
