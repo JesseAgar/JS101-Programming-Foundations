df2e03451328c4ede3b130d10a39e7032877e8f1
JesseWindows <agarjesse@gmail.com>
2023-03-21 14:11:11 -0400

    add E1-05 PECAC

---
+++ 1-Easy-1-2-3/E1-05-CalculateTip.js
@@ -0,0 +1,58 @@
+let readlineSync = require('readline-sync');
+
+function calculateTip() {
+  let bill = readlineSync.question("What is the bill?");
+  let tipPercent = readlineSync.question("What is the tip percentage (ex: 15%)?");
+  console.log(bill + bill );
+  console.log(
+}
+
+/* UNDERSTAND THE PROBLEM
+  - read the problem description 
+  - identify inputs/outputs
+  - identify rules
+  - identify requirements
+  - mental model problem
+  - ask clarifying questions
+  - examine all examples
+* INPUTS
+? 2 numbers, tip percent and bill cost
+* OUTPUTS
+? total including tip and just the tip
+* RULES AND REQUIREMENTS
+? Ignore invalid input 
+*/
+/* EXAMPLES/TEST CASES
+  - understand how the input translates to output
+  - identify edge cases
+  - create the test cases and confirm outputs
+* VALID CASES  */
+calculateTip();
+// * EDGE CASES
+
+/* DATA STRUCTURES
+  - what sort of actions do you have to do (sort, collect, filter, etc.)
+  - what kind of data are you primarily dealing with? (strings, arrays, numbers, objects, etc.)
+  - this helps you to focus on methods for these types
+? 
+
+*/
+/* ALGORITHM
+  - step by step process that takes you from input to output
+  - handles edges cases and valid example inputs
+  - your code will depend on your algorithm
+? 
+?
+?
+*/
+
+/* CODE
+  - time to write the code!
+  - use everything you've gathered in your pedac to write the code 
+
+  Debug (if needed)
+  - if there are errors in output, don't panic!
+  - carefully review algorithm before looking at your code
+  - once you've identified the issue, change the algorithm first, then fix the code
+  - try again!
+*/
