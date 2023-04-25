eb5324dc07af30017c3f2a0c58f38513523a2a6d
Jesse_HP1 <agarjesse@gmail.com>
2023-04-25 16:05:38 -0400

    finish Mortgage Loan Calculator

--- Mortgage-Loan-Calculator/MLC.js
+++ Mortgage-Loan-Calculator/MLC.js
@@ -1,74 +1,60 @@
-/*
-*UNDERSTAND THE PROBLEM
-  - read the problem description 
-  - examine all given examples for info
-  - identify INPUTS/OUTPUTS
-? the loan amount
-? the Annual Percentage Rate (APR) in percent (10%)
-? the loan duration in months
-  - identify RULES/REQUIREMENTS
-? monthly interest rate
-? calculate loan duration in months
-  - mental model problem. Sort of like a VERY simple algorithm. Just describe what you broadly need to do.
-? get input of the 3 numbers, then calculate the monthly loan amount
-  - ask clarifying questions
+const READLINESYNC = require('readline-sync');
+const MESSAGES = require('./MLC-messages.json');
+const INVALIDCHAR = require('./invalid-characters.json');
 
-*EXAMPLES/TEST CASES
-  - understand how the input translates to output
-  - identify VALID CASES
-? positive numbers all round
-? is a number
-  - identify EDGE CASES
-? numbers with $ or % in the name
-? negative numbers
-  - create the test cases and confirm outputs */
-
-
-  /* !DATA STRUCTURES
-    - what sort of actions do you have to do (sort, collect, filter, etc.)
-    - what kind of data are you primarily dealing with? (strings, arrays, numbers, objects, etc.)
-    - focus on methods for these types
-  ?
-  
-*ALGORITHM
-    - step by step process that takes you from input to output, your code will depend on your algorithm
-    - handles edges cases and valid example inputs 
-  ? Get loan amount
-  ? Check validity, get again if invalid
-  ? Get APR
-  ? Check validity, get again if invalid
-  ? get loan duration
-  ? Check validity, get again if invalid
-  ?
-  ? input numbers into equation
-  ? output results in a sentence
-*/
-
-let readlineSync = require('readline-sync');
-let messages = require('MLC-messages.json');
+mortgageLoanCalculator();
 
 function mortgageLoanCalculator() {
-  getLoan();
+  let principle = getLoan();
+  while (!isValidLoan(principle)) {
+    console.log(MESSAGES.invalidLoan);
+    principle = getLoan();
+  }
+
+  let apr = getAPR();
+  while (!isValidAPR(apr)) {
+    console.log(MESSAGES.invalidAPR);
+    apr = getAPR();
+  }
+  apr /= 100;
+
+  let duration = getDuration();
+  while (!isValidDuration(duration)) {
+    console.log(MESSAGES.invalidDuration);
+    duration = getDuration();
+  }
+
+  console.log( `your monthly payment is $ ${calcMonthlyMortgageLoan(principle, apr, duration)}`);
+
 
 }
 function getLoan() {
-
+  let dirtyLoan = READLINESYNC.question(MESSAGES.askLoan);
+  return dirtyLoan.trim().split('').filter(x => !INVALIDCHAR.loan.includes(x)).join('');
 }
 
-apple = 12;
-
-function isValidLoan() {
-
+function getAPR() {
+  let  dirtyAPR = READLINESYNC.question(MESSAGES.askAPR);
+  return dirtyAPR.trim().split('').filter(x => !INVALIDCHAR.APR.includes(x)).join('');
 }
 
-function isValidAPR() {
-
+function getDuration() {
+  let dirtyDuration = READLINESYNC.question(MESSAGES.askDuration);
+  return dirtyDuration.trim().split('').filter(x => !INVALIDCHAR.duration.includes(x)).join('');
 }
 
-function isValidMonths() {
-
+function isValidLoan(testLoan) {
+  return testLoan >= 0;
 }
 
-function calcMonthlyMortgageLoan() {
-  
+function isValidAPR(testAPR) {
+  return testAPR >= 0;
+}
+
+function isValidDuration(testDuration) {
+  return testDuration >= 0;
+}
+
+function calcMonthlyMortgageLoan(loan, APR, months) {
+  return (loan * (APR / (1 - ((1 + APR) ** (-months))))).toFixed(2);
 }
